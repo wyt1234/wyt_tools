116c389c24323a2379962a702ddac604d3c61027
wyt1234 <42131304+wyt1234@users.noreply.github.com>
2022-06-23 17:20:49 +0800

    Initial commit

---
+++ common_fragment/path_relevant.py
@@ -0,0 +1,15 @@
+############
+'''
+添加当前路径
+'''
+import sys
+import os
+
+curr_path = os.path.dirname(os.path.abspath(__file__))  # 当前文件所在绝对路径
+parent_path = os.path.dirname(curr_path)  # 父路径
+sys.path.append(parent_path)  # 添加路径到系统路径
+############
+
+
+
+
