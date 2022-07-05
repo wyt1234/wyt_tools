0281bed93abf7b4e3ce68894f6364936e30f9a83
wyt1234 <42131304+wyt1234@users.noreply.github.com>
2022-07-05 12:28:50 +0800

    log and redis

--- common_fragment/log_relevant.py
+++ common_fragment/log_relevant.py
@@ -1,3 +1,7 @@
 from loguru import logger
 
 logger.add("file_{time}.log")
+logger.add('runtime_{time}.log', rotation='00:00')
+logger.add('runtime_{time}.log', rotation='1 week')
+logger.add('runtime.log', retention='10 days')
+logger.add('runtime_{time}.log', rotation="500 MB")
---
+++ common_fragment/redis_relevent.py
@@ -0,0 +1,2 @@
+import redis
+r = redis.Redis(host='', password='', port=6379, db=0, decode_responses=True)
