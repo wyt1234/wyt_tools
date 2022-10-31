f93336f781ae734828442ad793e315e9855f14b5
wyt1234 <42131304+wyt1234@users.noreply.github.com>
2022-10-31 10:53:08 +0800

    influxdb

--- docker/influxdb/start.sh
+++ docker/influxdb/start.sh
@@ -1,5 +1,7 @@
+#https://docs.influxdata.com/influxdb/v2.4/install/?t=Docker#persist-data-outside-the-influxdb-container
+
 docker run \
-    --name influxdb \
-    -p 8086:8086 \
-      --volume /data/influxdb:/var/lib/influxdb2 \
-    influxdb:2.4.0
+  --name influxdb \
+  -p 8086:8086 \
+  --volume /data/influxdb:/var/lib/influxdb2 \
+  influxdb:2.4.0
